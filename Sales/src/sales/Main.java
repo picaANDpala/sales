1c95214194131209850d2afed5c85510374bd288
safin91 <safin91@hotmail.it>
2015-11-15 12:01:46 +0100

    create a class sales

---
+++ Sales/src/sales/Main.java
@@ -0,0 +1,12 @@
+package sales;
+
+public class Main {
+
+		public static void main(String[] args) {
+			Sales  sales = new Sales("mario");
+			Sales sales1 = new Sales("ciccio");
+			
+			sales.print();
+			sales1.print();
+		}
+}
---
+++ Sales/src/sales/Sales.java
@@ -0,0 +1,19 @@
+package sales;
+
+public class Sales {
+
+	private String name;
+	
+		public Sales() {
+			this.name = "";
+		}
+		
+		public Sales(String n){
+			this.name = n;
+		}
+		
+		public void print() {
+			System.out.println(name);
+
+		}
+}
