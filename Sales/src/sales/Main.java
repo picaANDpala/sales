3066f3bac4728a333d22c8201f143318facb7d58
EugenioGiuseppe <falkko19@hotmail.it>
2015-11-15 12:25:46 +0100

    i added a team sales

--- Sales/src/sales/Main.java
+++ Sales/src/sales/Main.java
@@ -5,8 +5,10 @@ public class Main {
 		public static void main(String[] args) {
 			Sales  sales = new Sales("mario");
 			Sales sales1 = new Sales("ciccio");
+			Sales sales2 = new Sales("pasticcio");
 			
 			sales.print();
 			sales1.print();
+			sales2.print();
 		}
 }
